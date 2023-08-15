9b44092f6d279207eec4739f636496792702691c
MdAbuNafeeIbnaZahid <nafee.zahid@Mds-MacBook-Pro.local>
2023-08-15 12:57:49 +0600

    wrote code for fetching by id

--- framework/vehicle_data.py
+++ framework/vehicle_data.py
@@ -2,7 +2,27 @@ import numpy
 
 
 class VehicleData:
-    def __init__(self, string):
-        data = numpy.load(string)
-        print(data)
+    def __init__(self, file_name):
+        matrix = numpy.load(file_name)
+        self._build_segments(matrix)
+
+    def _build_segments(self, matrix):
+        self.segments = dict()
+        for row in matrix:
+            idx = row[0]
+            id = row[1]
+            x = row[2]
+            y = row[3]
+            if id not in self.segments:
+                self.segments[id] = []
+            self.segments[id].append((x, y))
+        for id in self.segments:
+            if len(self.segments[id]) == 0:
+                del self.segments[id]
+
+    def by_id(self, id):
+        if id not in self.segments:
+            return None
+        return self.segments[id]
+
 
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from framework import vehicle_data
 
-
-
-
 if __name__ == '__main__':
-    data_file = './data.npy' # pass by command line interface
+    data_file = './data.npy'  # pass by command line interface
     obj = vehicle_data.VehicleData(data_file)
-
-
-
-
