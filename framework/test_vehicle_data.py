b9018207644b3338cdd911c53dc9cf064ec09a7c
MdAbuNafeeIbnaZahid <nafee.zahid@Mds-MacBook-Pro.local>
2023-08-16 06:39:29 +0600

    unit testing successfully written

--- framework/test_vehicle_data.py
+++ framework/test_vehicle_data.py
@@ -4,27 +4,31 @@ import numpy
 from framework import vehicle_data
 
 
-
 class TestVehicleData(TestCase):
-    class TestById(TestCase):
+    def setUp(self) -> None:
         matrix = [
             [1, 0, 2, 3],
             [2, 0, 3, 2],
             [3, 1, 3, 4]
         ]
-        file = numpy.save("./file", matrix)
-        vehicle_data = vehicle_data.VehicleData("./file.npy")
+        _ = numpy.save("./file", matrix)
+        self.v_data = vehicle_data.VehicleData("./file.npy")
         os.remove("./file.npy")
 
-        assert(vehicle_data.by_id(0) == [(2, 3), (3, 2)] )
-        assert(vehicle_data.by_id(1) == []) # only one datapoint. Got removed
+    def test_by_id_two_values(self):
+        assert (self.v_data.by_id(0) == [(2, 3), (3, 2)])
 
+    def test_by_id_single_id(self):
+        assert (self.v_data.by_id(1) == [])  # only one datapoint. Got removed
+
+    def test_filter_2_values(self):
         def len_is_2(trajectory):
             return len(trajectory) == 2
 
+        assert (self.v_data.filter(len_is_2) == [[(2, 3), (3, 2)]])
+
+    def test_filter_1_value_no_found(self):
         def len_is_1(trajectory):
             return len(trajectory) == 1
 
-        assert (vehicle_data.filter(len_is_2) == [ [(2, 3), (3, 2)] ])
-
-        assert (vehicle_data.filter(len_is_1) == []) # only one datapoint. Got removed
+        assert (self.v_data.filter(len_is_1) == [])  # only one datapoint. Got removed
